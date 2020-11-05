e4d7a0808e494a1ef597061109665646df4d825f
Erick <emayorga30@gmail.com>
2020-11-04 23:49:18 -0600

    V1 4-11

--- AnalizarTokens.py
+++ AnalizarTokens.py
@@ -1,9 +1,42 @@
 import re
+import os
 
 class CreaArchivo:
     tokens = []
     errores2 = []
     nodos = []
+    nombres = []
+
+    def grafica_lista(self, boolean, nombre_graph):
+        print("llego aca?")
+        file = open("Prueba.dot", "w")
+
+        file.write("digraph D {" + "\n")
+        file.write("rankdir= LR" + "\n")
+        for nodo in self.nodos:
+            file.write(nodo + "\n")
+
+        inicio = 0
+        numero_nombres= len(self.nombres)
+
+        while inicio < numero_nombres:
+            if boolean:
+                inicio += 1
+                if ((inicio + 1) < numero_nombres):
+                    file.write(self.nombres[inicio] + "->" + self.nombres[inicio + 1] + "\n")
+                    file.write(self.nombres[(inicio + 1)] + "<-" + self.nombres[inicio] + "\n")
+
+
+            else:
+                file.write(self.nombres[inicio] + "->" + self.nombres[inicio + 1] + "\n")
+                inicio += 1
+
+        file.write('label = "' + nombre_graph + '" ;' + "\n")
+        file.write("}")
+        file.close()
+        os.system('dot -Tsvg Lista.dot -o Lista.svg')
+
+        os.system("Lista.svg")
 
     def crea_grafica_lista(self):
         patron_numero = "[0-9]"
@@ -11,16 +44,18 @@ class CreaArchivo:
         p2 = ' ,style="filled", fillcolor= "'
 
         lista_doble = False
-        lista_simple = False
+
         color = ""
         forma = ""
-        nombre_mapa = self.tokens[3][1]
+        nombre_graph = self.tokens[3][1]
         for token in self.tokens:
+            print(token[1], token[4])
             if token[1].lower() == "verdadero":
                 lista_doble = True
             elif token[1].lower() == "falso":
-                lista_simple = True
+                lista_doble = False
             elif token[4].lower == "forma":
+                print("Entra aca?")
                 if token[1].lower() == "circulo":
                     forma = "circle"
                 elif token[1].lower() == "triagunlo":
@@ -38,7 +73,7 @@ class CreaArchivo:
         self.nodos.append(codigo_forma)
 
         for x in range(0, len(self.tokens)):
-            print(self.tokens[x][1])
+
 
             if self.tokens[x][1] == "nodo":
                 if re.search(patron_numero, self.tokens[(x+2)][1]):
@@ -94,10 +129,14 @@ class CreaArchivo:
                         color = "white"
                     n_nodos = 0
 
-                    while n_nodos < self.tokens[x+2][1]:
+                    while n_nodos < int(self.tokens[x+2][1]):
                         nodo = self.tokens[(x + 5)][1] + p1 + self.tokens[(x + 5)][1] + str((n_nodos+1)) + '"' + p2 + color + '"' + "]"
                         self.nodos.append(nodo)
 
+                        nombre =self.tokens[(x+5)][1] + str((n_nodos + 1))
+                        self.nombres.append(nombre)
+
+                        n_nodos += 1
 
                 else:
                     if self.tokens[(x + 6)][1].lower() == "azul":
@@ -131,9 +170,9 @@ class CreaArchivo:
                     elif self.tokens[(x + 6)][1].lower() == "cafe3":
                         color = "saddlebrown"
                     elif self.tokens[(x + 6)][1].lower() == "gris":
-                        color = "gray97"
+                        color = "grey97"
                     elif self.tokens[(x + 6)][1].lower() == "gris2":
-                        color = "gray60"
+                        color = "grey60"
                     elif self.tokens[(x + 6)][1].lower() == "gris3":
                         color = "gray24"
                     elif self.tokens[(x + 6)][1].lower() == "morado":
@@ -153,6 +192,10 @@ class CreaArchivo:
 
                     nodo = self.tokens[(x+3)][1] + p1 + self.tokens[(x+3)][1] + '"' + p2 + color + '"' + "]"
                     self.nodos.append(nodo)
+                    nombre = self.tokens[(x+3)][1]
+                    self.nombres.append(nombre)
+
+        self.grafica_lista(lista_doble, nombre_graph)
 
 
     def escribeinfo(self, info, file, segundaCol, UltimaCol):
@@ -166,8 +209,9 @@ class CreaArchivo:
                     file.write(";")
                 else:
                     file.write(str(info[x][y]))
-                    print(str([x]) + str([y]))
                     file.write(";")
+
+
 
 
 
@@ -185,6 +229,6 @@ class CreaArchivo:
         self.tokens = tokens
         self.errores2 = errores
         #self.crearcsv(self.tokens, self.errores2)
-        print(self.tokens[0][1])
+
         if self.tokens[0][1] == "lista":
             self.crea_grafica_lista()
